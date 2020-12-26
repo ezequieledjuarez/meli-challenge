c134866506d1e5b559206b4f5f6e9fb33b2b410f
EzeJuarez <ezequieledjuarez@gmail.com>
2020-12-26 10:49:31 -0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,50 @@
+var express = require('express');
+var exphbs  = require('express-handlebars');
+var port = process.env.PORT || 3000
+
+var app = express();
+
+// SDK de Mercado Pago
+const mercadopago = require ('mercadopago');
+
+// Agrega credenciales
+mercadopago.configure({
+  access_token: 'PROD_ACCESS_TOKEN'
+});
+
+// Crea un objeto de preferencia
+let preference = {
+  items: [
+    {
+      title: 'Mi producto',
+      unit_price: 100,
+      quantity: 1,
+    }
+  ]
+};
+
+mercadopago.preferences.create(preference)
+.then(function(response){
+// Este valor reemplazará el string "<%= global.id %>" en tu HTML
+  global.id = response.body.id;
+}).catch(function(error){
+  console.log(error);
+});
+
+ 
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+app.use(express.static('assets'));
+ 
+app.use('/assets', express.static(__dirname + '/assets'));
+
+app.get('/', function (req, res) {
+    res.render('home');
+});
+
+app.get('/detail', function (req, res) {
+    res.render('detail', req.query);
+});
+
+app.listen(port);
